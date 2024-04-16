a20a82b39eed4ee7e53085da1aef64c7273e529d
scomeras94 <scomeras@gmail.com>
2024-04-16 06:08:01 +0200

    ADD - Pdf Viewer menu item

--- sco-angular-components-base/src/app/modules/shared/menu/menu.constants.ts
+++ sco-angular-components-base/src/app/modules/shared/menu/menu.constants.ts
@@ -1,8 +1,9 @@
 import { MenuItem } from "sco-angular-components";
-import { LOGIN_MENU_ITEM, LOGOUT_MENU_ITEM, OPTIONS_MENU_ITEM, RESUME_MENU_ITEM, USERS_MENU_ITEM } from "./menu-item.constants";
+import { LOGIN_MENU_ITEM, LOGOUT_MENU_ITEM, OPTIONS_MENU_ITEM, PDF_EXAMPLE_MENU_ITEM, RESUME_MENU_ITEM, USERS_MENU_ITEM } from "./menu-item.constants";
 
 export const MENU_SUPER_ADMIN: MenuItem[] = [
   RESUME_MENU_ITEM,
+  PDF_EXAMPLE_MENU_ITEM,
   USERS_MENU_ITEM,
   OPTIONS_MENU_ITEM,
   LOGOUT_MENU_ITEM,
@@ -10,6 +11,7 @@ export const MENU_SUPER_ADMIN: MenuItem[] = [
 
 export const MENU_ADMIN: MenuItem[] = [
   RESUME_MENU_ITEM,
+  PDF_EXAMPLE_MENU_ITEM,
   USERS_MENU_ITEM,
   OPTIONS_MENU_ITEM,
   LOGOUT_MENU_ITEM,
@@ -17,6 +19,7 @@ export const MENU_ADMIN: MenuItem[] = [
 
 export const MENU_USER: MenuItem[] = [
   RESUME_MENU_ITEM,
+  PDF_EXAMPLE_MENU_ITEM,
   OPTIONS_MENU_ITEM,
   LOGOUT_MENU_ITEM,
 ];
